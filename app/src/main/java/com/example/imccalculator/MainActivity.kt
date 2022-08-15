d0abc7364ab908a284da745521beae4f86cd146c
Ariel Alves <arielalves9@gmail.com>
2022-08-15 14:32:59 -0300

    add result activity and layout

--- app/src/main/java/com/example/imccalculator/MainActivity.kt
+++ app/src/main/java/com/example/imccalculator/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.imccalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+     private val alturaEDT: TextView? = findViewById(R.id.alturaEDT)
+     private val pesoEDT: TextView? = findViewById(R.id.pesoEDT)
+     private val calcularBTN: Button? = findViewById(R.id.calcularBTN)
+     private val titleTXT: TextView? = findViewById(R.id.titleTXT)
+
+    private fun setListeners() {
+        alturaEDT?.doAfterTextChanged { text ->
+           // Toast.makeText(this,text.toString(), Toast.LENGTH_SHORT).show()
+        }
+        pesoEDT?.doOnTextChanged{ text, _, _, _ ->
+
+        }
+        calcularBTN?.setOnClicListener {
+            calcularIMC(pesoEDT.text.toString(), alturaEDT.text.toString)
+        }
+    }
+    private fun calcularIMC (peso: String, altura: String) {
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+        if (peso != null && altura != null){
+         val imc = peso / (altura * altura)
+         titleTXT.text = "Seu IMC é de: $imc.2f".format(imc)
+        }
     }
 }
---
+++ app/src/main/java/com/example/imccalculator/ResutActivity.kt
@@ -0,0 +1,16 @@
+package com.example.imccalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class ResutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resut)
+    }
+
+
+}
