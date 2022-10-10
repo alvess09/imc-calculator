e0652c6a7a78582ebdcbc523316985c33b79827f
Ariel Alves <arielalves9@gmail.com>
2022-10-10 16:26:07 -0300

    update ResultActivity

--- app/src/main/java/com/example/imccalculator/MainActivity.kt
+++ app/src/main/java/com/example/imccalculator/MainActivity.kt
@@ -1,62 +1,48 @@
 package com.example.imccalculator
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.imccalculator.R.layout.activity_main
-import com.santalu.maskara.Mask
-import com.santalu.maskara.MaskChangedListener
-import com.santalu.maskara.MaskStyle
 
 class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_main)
 
-        val peso: String = findViewById<TextView>(R.id.pesoTXT).toString()
-        val altura :String = findViewById<TextView>(R.id.alturaTXT).toString()
-        val imc = Imc(peso.toFloat(), altura.toFloat()).apply {
-            indice()
-            calculoImc(peso.toFloat(), altura.toFloat())
-        }
+        //passando os valores recebidos para String
+        val peso: String = R.id.pesoTXT.toString()
+        val altura: String = R.id.alturaTXT.toString()
 
-        val mascaraAltura = Mask(
-            value = "_,__",
-            character = '_',
-            style = MaskStyle.COMPLETABLE
-        )
-        val mascaraPeso = Mask(
-            value = "___,__",
-            character = '_',
-            style = MaskStyle.COMPLETABLE
-        )
-        val listener = MaskChangedListener(mascaraPeso)
-        val listenerAltura = MaskChangedListener(mascaraAltura)
-        peso.addTextChangedListener(listener)
-        altura.addTextChangedListener(listenerAltura)
+        /* executar quando clicar no botão calcular */
 
+        val btnCalcular = findViewById<Button>(R.id.calcularBTN).setOnClickListener {
 
-
-
-
-
-        //val calculoImc: String = verificaIndice(imc)
-
-
-       // val duracao =  Toast.LENGTH_SHORT
-        //Toast.makeText(this,calculoImc, duracao).show()
-
-       /* val btnCalcular = findViewById<Button>(R.id.calcularBTN).setOnClickListener {
             val intent = Intent(this, ResultActivity::class.java)
             //passando resultado para activity de resultado
-            intent.putExtra("resultadoIndice",calculoImc)
-            intent.putExtra("numeroImc",imc.roundToInt())
+            intent.putExtra("peso", peso)
+            intent.putExtra("altura", altura)
             startActivity(intent)
-        }*/
+
+            Log.i("edit","valor de peso: $peso " +
+                    ", valor de altura: $altura,")
+        }
 
     }
 
+
+
+
+
 }
+
+
+
+
+
+
 
 
 
--- app/src/main/java/com/example/imccalculator/ResultActivity.kt
+++ app/src/main/java/com/example/imccalculator/ResultActivity.kt
@@ -1,5 +1,6 @@
 package com.example.imccalculator
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
@@ -14,55 +15,106 @@ class ResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(activity_resut)
 
-        val dados = intent.extras
-        val resultadoIndice  = dados?.getString("resultadoIndice")
+        val dados = this.intent.extras
+        val peso = dados?.getString("peso")
+        val altura = dados?.getString("altura")
 
-        val numeroResultadoImc = dados?.getInt("numeroImc")
+        val imc = calcularImc(peso.toString(), altura.toString())
+
+        val indiceResultado = calculaIndice(imc)
 
 
 
-        val numeroImc = findViewById<TextView>(R.id.tv_result_number)
-        numeroImc.text = numeroResultadoImc.toString()
+
+
 
         //configurar a cor do circulo de acordo com o resultado
-        alteraCorDoCirculo(resultadoIndice)
+        alteraCorDoCirculo(indiceResultado)
 
         val duration =  Toast.LENGTH_SHORT
-        Toast.makeText(this, resultadoIndice, duration).show()
+        Toast.makeText(this, indiceResultado, duration).show()
 
+
+    }
+    @SuppressLint("SetTextI18n")
+    private fun calcularImc(peso: String, altura: String) :Float{
+
+        val pesoConvertido = peso.toFloatOrNull()
+        val alturaConvertida = altura.toFloatOrNull()
+        var imc = 0.0F
+        val numeroImc = findViewById<TextView>(R.id.tv_result_number)
+
+        if (pesoConvertido != null && alturaConvertida != null){
+            imc = pesoConvertido / (alturaConvertida * alturaConvertida)
+
+            numeroImc.text = " %.2f".format(imc)
+        }
+       return imc
+    }
+
+    private fun calculaIndice(imc:Float):String {
+        var resultado = ""
+
+        /* menor que 18,5 abaixo do peso ideal
+           18,5 a 24,9 peso normal
+           25,0 a 29,9 Acima do peso
+           30,0 a 34,9 Obesidade classe 1
+           35,0 a 39,9 Obesidade classe 2
+           imc <= 40,0 Obesidade classe 3
+         fonte OMS */
+
+        if(imc < 18.49){
+            "low".also { resultado = it }
+        }
+        if(imc > 18.5 && imc <= 24.99){
+            "normal".also { resultado = it }
+        }
+        if(imc > 25.0 && imc <= 29.99){
+            "high".also { resultado = it }
+        }
+        if(imc > 30.0 && imc <= 34.99){
+            "obesity1".also { resultado = it }
+        }
+        if(imc > 35.0 && imc <= 39.99){
+            "obesity2".also { resultado = it }
+        }
+        if(imc < 40.0){
+            "obesity3".also { resultado = it }
+        }
+        return resultado
 
     }
 
 
-private fun alteraCorDoCirculo(resultado: String?){
+    private fun alteraCorDoCirculo(resultado: String?){
 
-    val circle = findViewById<ImageView>(R.id.ic_circle)
+        val circle = findViewById<ImageView>(R.id.ic_circle)
 
-    when (resultado) {
-        "low" -> {
-             circle.setColorFilter(ContextCompat.getColor(this, R.color.low_weight), android.graphics.PorterDuff.Mode.SRC_IN)
+        when (resultado) {
+            "low" -> {
+                circle.setColorFilter(ContextCompat.getColor(this, R.color.low_weight), android.graphics.PorterDuff.Mode.SRC_IN)
+            }
+            "normal" -> {
+                circle.setColorFilter(ContextCompat.getColor(this, R.color.normal_weight), android.graphics.PorterDuff.Mode.SRC_IN)
+            }
+            "high" -> {
+                circle.setColorFilter(ContextCompat.getColor(this, R.color.high_weight), android.graphics.PorterDuff.Mode.SRC_IN)
+            }
+            "obesity1" -> {
+                circle.setColorFilter(ContextCompat.getColor(this, R.color.obesity_1_weight), android.graphics.PorterDuff.Mode.SRC_IN)
+            }
+            "obesity2" -> {
+                circle.setColorFilter(ContextCompat.getColor(this, R.color.obesity_2_weight), android.graphics.PorterDuff.Mode.SRC_IN)
+            }
+            "obesity3" -> {
+                circle.setColorFilter(ContextCompat.getColor(this, R.color.obesity_3_weight), android.graphics.PorterDuff.Mode.SRC_IN)
+            }
         }
-        "normal" -> {
-             circle.setColorFilter(ContextCompat.getColor(this, R.color.normal_weight), android.graphics.PorterDuff.Mode.SRC_IN)
-        }
-        "high" -> {
-            circle.setColorFilter(ContextCompat.getColor(this, R.color.high_weight), android.graphics.PorterDuff.Mode.SRC_IN)
-        }
-        "obesity1" -> {
-            circle.setColorFilter(ContextCompat.getColor(this, R.color.obesity_1_weight), android.graphics.PorterDuff.Mode.SRC_IN)
-        }
-        "obesity2" -> {
-            circle.setColorFilter(ContextCompat.getColor(this, R.color.obesity_2_weight), android.graphics.PorterDuff.Mode.SRC_IN)
-        }
-        "obesity3" -> {
-            circle.setColorFilter(ContextCompat.getColor(this, R.color.obesity_3_weight), android.graphics.PorterDuff.Mode.SRC_IN)
-        }
+
     }
+
 
 }
-
-
-    }
 
 
 
